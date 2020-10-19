cf521eb77a386ccba1b189134578f95dfca9e472
Dmitry-Valyaykin <72880960+Dmitry-Valyaykin@users.noreply.github.com>
2020-10-19 19:50:03 +0300

    lesson2 tesk 1-6

---
+++ Homework/lesson2/task2.py
@@ -0,0 +1,21 @@
+"""
+Для списка реализовать обмен значений соседних элементов, т.е.
+ Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+  При нечетном количестве элементов последний сохранить на своем месте.
+   Для заполнения списка элементов необходимо использовать функцию input().
+"""
+name_list = input('напишите слово:>>> ')
+name_list = list(name_list)
+#print(name_list)
+#new_name = []
+#result_list.insert(1, "ins_el")
+
+#name_list[0], name_list[1] = name_list[1], name_list[0]
+#for i in range(len(name_list)):
+    #for el in name_list:
+        #el = name_list[i+1]
+        #name_list.insert(i,el)
+        #new_name = name_list
+    #print(name_list)
+#el[1], el[i+1] = el[i+1], el[i]
+#print(name_list)
